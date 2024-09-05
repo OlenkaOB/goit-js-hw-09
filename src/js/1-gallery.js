76548043b14ee0163997f659b034352874bb25d0
Olena Borodkina <elena.kaminnik@gmail.com>
2024-09-05 22:15:30 +0300

    lightbox

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,8 @@
-console.log('Gallery');
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const Lightbox = new SimpleLightbox('.some-element a', {
+  /* options */
+});
+console.log(SimpleLightbox);
